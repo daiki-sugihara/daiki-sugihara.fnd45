89d5c63c7d2b4ab264a71f87df359b3013cda6f7
杉原大騎 <daiki_sugihara@mail.toyota.co.jp>
2026-03-24 18:58:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const clubRanges = [
-  { name: "DR(ドライバー)",         min: 210, max: 260 },
-  { name: "5W(5番ウッド)",          min: 190, max: 220 },
-  { name: "4U(4番ユーティリティ)",   min: 180, max: 205 },
-  { name: "5U(5番ユーティリティ)",   min: 165, max: 195 },
-  { name: "5I(5番アイアン)",         min: 175, max: 190 },
-  { name: "6I(6番アイアン)",         min: 160, max: 175 },
-  { name: "7I(7番アイアン)",         min: 145, max: 160 },
-  { name: "8I(8番アイアン)",         min: 130, max: 145 },
-  { name: "9I(9番アイアン)",         min: 115, max: 130 },
-  { name: "PW(ピッチングウェッジ)",   min: 100,  max: 115 },
-  { name: "AW(アプローチウェッジ)",   min: 80,  max: 100 },
-  { name: "SW(サンドウェッジ)",      min: 10,  max: 80  },
-  { name: "PT(パター)",              min: 0,   max: 10  }
+  { name: "DR(ドライバー)", min: 210, max: 260 },
+  { name: "5W(5番ウッド)", min: 190, max: 220 },
+  { name: "4U(4番ユーティリティ)", min: 180, max: 205 },
+  { name: "5U(5番ユーティリティ)", min: 165, max: 195 },
+  { name: "5I(5番アイアン)", min: 175, max: 190 },
+  { name: "6I(6番アイアン)", min: 160, max: 175 },
+  { name: "7I(7番アイアン)", min: 145, max: 160 },
+  { name: "8I(8番アイアン)", min: 130, max: 145 },
+  { name: "9I(9番アイアン)", min: 115, max: 130 },
+  { name: "PW(ピッチングウェッジ)", min: 100, max: 115 },
+  { name: "AW(アプローチウェッジ)", min: 80, max: 100 },
+  { name: "SW(サンドウェッジ)", min: 10, max: 80 }
 ];
 
 
+function calcWind(yards) {
+  const wind = document.getElementById("windInput").valueAsNumber;
+  if (!wind)
+    return yards;
+  else {
+    return yards + (wind * 3)
+  };
+}
+
+
+function calcSlope(yards) {
+  const slope = document.getElementById("slope").valueAsNumber;
+  if (!slope)
+    return yards;
+  else {
+    return yards + slope
+  };
+}
+
 
 function selectClub(yards) {
-  let bestClub = "SW(サンドウェッジ)";     
-  let bestDiff = 1000;    
+  let bestClub = clubRanges[0];
+  let bestDiff = 9999;
 
-  for (const club of clubRanges) {  
-    const average = (club["min"] + club["max"]) / 2;     
-    const diff   = Math.abs(yards - average);      
+  for (const club of clubRanges) {
+    const average = (club["min"] + club["max"]) / 2;
+    const diff = Math.abs(yards - average);
 
     if (diff < bestDiff) {
-      bestDiff = diff;   
-      bestClub = club;   
+      bestDiff = diff;
+      bestClub = club;
     }
   }
   return bestClub;
@@ -34,16 +52,20 @@ function selectClub(yards) {
 
 
 function showResult(yards, club) {
-  const resultElemnt = document.getElementById("result");
-  resultElemnt.innerHTML =  "<p> 残り" + yards + "ydのおすすめ：" + club["name"] + "</p>"   //HTML内のresultタグ(resultElement)の内容を「残り～」に書き換える。
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML = "残り " + yards + "yd のおすすめクラブは" + club.name + "です。";
 }
 
 
-
 function decide() {
-  const yards = document.getElementById("yardInput").valueAsNumber; // .valueAsNumberは要素の値を取り出して、数値に変換
+  let yards = document.getElementById("yardInput").valueAsNumber;
+
+  yards = calcWind(yards);
+  yards = calcSlope(yards);
+
   const club = selectClub(yards);
-  showResult(yards, club);  //前段で定義したshowResult関数を実行
+
+  showResult(Math.round(yards), club);
 }
 
 
@@ -52,15 +74,31 @@ function rangeList() {
   let html = "";
   for (const club of clubRanges) {
     html +=
-      "<div class='range'>" + club["name"] + "  :  "  + club["min"] + "〜" + club["max"] + " yd" + "</div>";
+      '<div class="range">' + club["name"] + "  :  " + club["min"] + "〜" + club["max"] + " yd" + "</div>";
   }
   rangeListElement.innerHTML = html;
 }
-
 
 const buttonElement = document.getElementById("decideBtn");
 buttonElement.addEventListener("click", function () {
   decide();
 });
-
 rangeList();
+
+
+
+
+
+function calcScore() {
+  let total = 0;
+
+
+  for (let i = 1; i <= 18; i++) {
+    const scoreValue = document.getElementById("score" + i).valueAsNumber;
+
+    if (scoreValue) total += scoreValue;
+  }
+
+  document.getElementById("totalScore").innerHTML =
+    "合計スコア：" + total + " 打";
+}
